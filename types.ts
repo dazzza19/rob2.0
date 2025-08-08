98cba11e1007e9f1ee244d420993e16b5847dfad
dazzza19 <31905056+dazzza19@users.noreply.github.com>
2025-08-08 11:40:09 +0100

    Update types.ts

--- types.ts
+++ types.ts
@@ -7,16 +7,17 @@ export interface CarInfo {
   description: string;
 }
 
-export interface DiagnosisResult {
-  title: string;
-  url: string;
-  description: string;
-}
-
 export interface Video {
   title: string;
   summary: string;
   url: string;
+}
+
+export interface DiagnosisResult {
+  title: string;
+  url: string;
+  description: string;
+  videos: Video[];
 }
 
 export interface DiagnoseRequest {
@@ -30,7 +31,5 @@ export enum AppState {
   IDLE,
   DIAGNOSING,
   DIAGNOSED,
-  FETCHING_VIDEOS,
-  VIDEOS_FOUND,
   ERROR,
 }
